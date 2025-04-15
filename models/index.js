80afc2de90572429978d8a97739a56c76fe39c07
FreshwayMedia <info@freshwaymedia.com>
2025-04-15 11:09:37 -0500

    Fix schema registration and index creation issues

--- models/index.js
+++ models/index.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const userSchema = require('./User');
 
 // Register the User model
-mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
 // Export the models
 module.exports = {
-    User: mongoose.model('User')
+    User
 }; 
